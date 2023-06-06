5871917f2d5f777ec7af34e43c4ab6c698536e96
Shagun-Shah <shagunshah259@gmail.com>
2023-06-06 16:46:36 +0200

    Made changes for correct feedback

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -5,19 +5,23 @@ import sqlite3
 dtypes = {'EVA_NR': int, 'DS100': str, 'IFOPT': str, 'NAME': str, 'Verkehr': str, 'Laenge': float, 'Breite': float, 'Betreiber_Name': str, 'Betreiber_Nr': float, 'Status': str }
 csv_df = pd.read_csv('https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV', sep=';', decimal=',', dtype=dtypes)
 
-# Drop a specific column from the DataFrame
-csv_df = csv_df.drop('Status', axis=1)  
+# First, drop the "Status" column
+csv_df = csv_df.drop('Status', axis=1)
 
-# Define the valid "Verkehr" values
+#Drop all rows with invalid values:
+#1. Valid "Verkehr" values are "FV", "RV", "nur DPN"
 valid_verkehr_values = ["FV", "RV", "nur DPN"]
 
 # Drop rows with invalid "Verkehr" values
 csv_df = csv_df[csv_df["Verkehr"].isin(valid_verkehr_values)]
 
-# Valid "Laenge", "Breite" values are geographic coordinate system values between -90 and 90
+# 2. Valid "Laenge", "Breite" values are geographic coordinate system values between -90 and 90
 # Remove columns with invalid values
 csv_df = csv_df[(csv_df['Laenge'] >= -90) & (csv_df['Laenge'] <= 90)]
 csv_df = csv_df[(csv_df['Breite'] >= -90) & (csv_df['Breite'] <= 90)]
+
+#3. Valid "IFOPT" values follow this pattern:
+#<exactly two characters>:<any amount of numbers>:<any amount of numbers><optionally another colon followed by any amount of numbers>
 
 # Define the regex pattern for valid "IFOPT" values
 pattern = r'^[A-Za-z]{2}:\d+:\d+(?::\d+)?$'
@@ -25,10 +29,7 @@ pattern = r'^[A-Za-z]{2}:\d+:\d+(?::\d+)?$'
 # Filter rows based on the specified "IFOPT" pattern
 csv_df = csv_df[csv_df['IFOPT'].str.contains(pattern, na=False)]
 
-# Fill values in Betreiber_Nr and convert to int
-csv_df = csv_df.fillna(value={'Betreiber_Nr': 0})
-csv_df['Betreiber_Nr'] = csv_df['Betreiber_Nr'].astype(int)
-
+#4. empty cells
 csv_df = csv_df.dropna()
 
 # Create a SQLite database and connect to it
