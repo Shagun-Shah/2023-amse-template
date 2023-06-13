3e29120ea92420e7015fc590001a45d52eda7915
Shagun-Shah <shagunshah259@gmail.com>
2023-06-13 13:36:56 +0200

    Made changes to path

--- project/tests.sh
+++ project/tests.sh
@@ -2,9 +2,9 @@
 
 # execute the pipeline
 echo "Execute the pipeline"
-python ../data/pipeline.py
+pytest -v data/pipeline.py
 
 
 # test if pipeline works correct
 echo "Test if pipeline works correctly"
-python -m pytest ../data/test_script.py
+python -m pytest data/test_script.py
