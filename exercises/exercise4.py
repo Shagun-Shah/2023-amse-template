5e47a44207a9d48b40b1f7a273450b41b7054b1d
Shagun-Shah <shagunshah259@gmail.com>
2023-06-30 16:29:23 +0200

    Completed exercise 4

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -13,7 +13,9 @@ with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extract(csv_file)
 
 # 2: Reshape data
+#Only use columns
 columns = ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"]
+#Rename column names
 new_column_names = {"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"}
 
 df = pd.read_csv(csv_file, delimiter=';', index_col=False, usecols=columns)
@@ -23,8 +25,6 @@ df.rename(columns=new_column_names, inplace=True)
 # Celsius to Fahrenheit
 df["Temperatur"] = df["Temperatur"].astype(str).apply(lambda x: float(x.replace(',', '.')))
 df["Batterietemperatur"] = df["Batterietemperatur"].astype(str).apply(lambda x: float(x.replace(',', '.')))
-
-# Celsius to Fahrenheit
 df["Temperatur"] = (df["Temperatur"] * 9/5) + 32
 df["Batterietemperatur"] = (df["Batterietemperatur"] * 9/5) + 32
 
@@ -44,7 +44,7 @@ df = df[temp_valid]
 valid_battery_range = df["Batterietemperatur"].between(*valid_temp_range)
 df = df[valid_battery_range]
 
-# 5: Write data into SQLite database
+# 5: Use fitting SQLite types & Write data into SQLite database
 df.to_sql('temperatures', 'sqlite:///temperatures.sqlite', if_exists='replace', index=False, dtype={
         'Geraet': BIGINT,
         'Hersteller': TEXT,
