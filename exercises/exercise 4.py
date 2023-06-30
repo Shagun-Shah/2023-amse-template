da347a4c4cadacda7fd7b614aef2ec7c0288c584
Shagun-Shah <shagunshah259@gmail.com>
2023-06-30 16:12:18 +0200

    Exercise 4 updated

--- exercises/exercise 4.py
+++ exercises/exercise 4.py
@@ -1,22 +1,23 @@
-import os
-import sqlite3
 import zipfile
 import urllib.request
 import pandas as pd
-from sqlalchemy import BIGINT, TEXT, FLOAT
+import sqlite3
 
 # 1: Download and unzip data
-zip_file, headers = urllib.request.urlretrieve('https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip')
+zip = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
+zip_path = "mowesta-dataset.zip"
+csv_file = "data.csv"
 
-with zipfile.ZipFile(zip_file, "r") as zip_ref:
-    target_dir = "data"
-    zip_ref.extractall(target_dir)
-csv_file = os.path.join(target_dir, os.listdir(target_dir)[0])
+urllib.request.urlretrieve(zip, zip_path)
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extract(csv_file)
 
 # 2: Reshape data
-df = pd.read_csv(csv_file, sep=';', decimal=',', index_col=False, usecols=["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"])
+columns = ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"]
+new_column_names = {"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"}
 
-df = df.rename(columns={'Temperatur in °C (DWD)': 'Temperatur','Batterietemperatur in °C': 'Batterietemperatur'})
+df = pd.read_csv(csv_file, delimiter=';', index_col=False, usecols=columns)
+df.rename(columns=new_column_names, inplace=True)
 
 # 3: Transform data
 # Celsius to Fahrenheit
@@ -27,14 +28,25 @@ df["Batterietemperatur"] = df["Batterietemperatur"].astype(str).apply(lambda x: 
 df["Temperatur"] = (df["Temperatur"] * 9/5) + 32
 df["Batterietemperatur"] = (df["Batterietemperatur"] * 9/5) + 32
 
-# 4: Validate 
-df = df[ (df['Monat'] >= 1) & (df['Monat'] <= 12)]
-df = df[ (df['Geraet'] >= 0)]
+# 4: Validate data
+# Data Type Validation
+df = df.astype({
+    "Geraet": int,
+    "Monat": int,
+    "Geraet aktiv": str })
 
-# Step 5: Write data to SQLite database
-database_name = "temperatures.sqlite"
+# Fahrenheit Temperature Validation
+valid_temp_range = (-459.67, 212)
+temp_valid = df["Temperatur"].between(*valid_temp_range)
+df = df[temp_valid]
+
+# Batterietemperatur Validation
+valid_battery_range = df["Batterietemperatur"].between(*valid_temp_range)
+df = df[valid_battery_range]
+
+# 5: Write data to SQLite database
+db_name = "temperatures.sqlite"
 table_name = "temperatures"
-
-connection = sqlite3.connect(database_name)
+connection = sqlite3.connect(db_name)
 df.to_sql(table_name, connection, if_exists='replace', index=False)
 connection.close()
